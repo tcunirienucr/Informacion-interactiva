29f89ea5fc8768fadab6c5b66603157a80c0cec7
tcunirienucr <tc665.economia@gmail.com>
2025-11-07 16:53:27 -0600

    Update appv2.py

--- appv2.py
+++ appv2.py
@@ -6,8 +6,8 @@ from streamlit_folium import st_folium
 from streamlit_gsheets import GSheetsConnection
 import plotly.express as px
 
-ruta_mapa="limitecantonal_5k.json"
-columna_mapa="CANTÓN"
+ruta_mapa="costaricacantonesv10.geojson"
+columna_mapa="NAME_2"
 
 # ===============================
 # Diccionario para mostrar nombres amigables
